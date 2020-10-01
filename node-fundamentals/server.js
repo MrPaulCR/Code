180285048b50621ab7a20d1fa05d6729f4fc8a7e
Paul Cando <paul-cando9871@hotmail.com>
2020-10-01 18:51:32 -0500

    Agregando a rama develop servidor.js

--- node-fundamentals/server.js
+++ node-fundamentals/server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const response = require('./network/response');
+
 const router = express.Router();
 
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
+
 router.get('/carreras', function (req, res) {
-    res.send("Lista de carreras de la Ups.");
+    
+    response.success(req,res,'Lista de carreras de la Ups.',200);
+    
+
 })
 router.post('/carreras', function (req, res) {
-    console.log(req.body);
-    console.log(req.query);
-    res.status(201).send({ tipo_error: 0, mensaje_error: '', mensaje_exito: 'Añadido Exitosamente.' });
+    if (req.query.error == 'ok') {
+    response.error(req,res,'Error al ingresar la carrera',500);
+    } else {
+        response.success(req,res,'Ingreso de carrera exitoso.',200);
+    }
 })
 
 app.listen(5000);
