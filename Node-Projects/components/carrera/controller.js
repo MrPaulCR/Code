447f260ff94d5521d8d2795c949f654b0d144c08
Paul Cando <paul-cando9871@hotmail.com>
2020-10-02 01:16:14 -0500

    Archivos del crud en rama develop

---
+++ Node-Projects/components/carrera/controller.js
@@ -0,0 +1,21 @@
+const storage = require('./storage');
+function addCarrera(nombre, descripcion) {
+    return new Promise((resolve, reject) => {
+        let carrera = {
+            nombre: nombre,
+            descripcion: descripcion,
+        }
+        storage.add(carrera);
+        resolve(carrera);
+    });
+}
+
+function getCarreras() {
+    return new Promise((resolve, reject) => {
+        resolve(storage.get())
+    });
+}
+
+module.exports = {
+    getCarreras,
+}   
